db6683146a6b8f0b0e3bb4c12dacb6883b8f825f
DanaValen <danavalenmartinez@gmail.com>
2024-05-09 16:57:28 -0500

    Se crearon las diferentes vistas del proyecto y se creo una migracion para la tabla en la base de django

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,120 @@
-from django.shortcuts import render
+from asyncio import Task
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
 
+def signup(request):
+    
+     if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form':UserCreationForm
+    })
+     else:
+         
+        if request.POST['password1'] == request.POST['password2']:
+             #register user 
+                try: 
+                    user= User.objects.create_user(username=request.POST['username'], 
+                        password=request.POST['password1'])
+                    user.save()
+                    login (request,user)
+                    return redirect('tasks')
+                except IntegrityError:
+                    return render(request, 'signup.html', {
+                        'form': UserCreationForm,
+                        "Error": 'El usuario ya existe'})
+                    
+        return render(request, 'signup.html', {
+                        'form': UserCreationForm,
+                        "Error": 'Contraseñas no coinciden'})
+        
+@login_required
+def tasks(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render (request, 'tasks.html',{'task': tasks})
+
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False)
+    return render (request, 'tasks.html',{'task': tasks})
+
+@login_required
+def create_task(request):
+    if request.method == "GET":
+        return render(request, 'create_task.html', {'form': TaskForm})
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {'form': TaskForm, 'Error': "Error, provee datos validos."})
+
+@login_required
+def task_defail(request, task_id):
+    if request.method =='GET':
+        task = task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render (request, 'task_detail.html', {'tasks': task, 'form': form})
+    else: 
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except:
+            return render(request, 'task_detail.html',{
+                'task': task, 'form': form,'Error': 'Error al actualizar'
+            })
+         
+def signout(request):
+    logout(request) 
+    return redirect('home')
+
+def signin (request):
+   if request.method == 'GET':
+        return render (request,'signin.html', {
+        'form': AuthenticationForm
+    })
+     
+   else:
+       user=authenticate(request, username=request.POST['username'], 
+            password=request.POST['password'])
+       
+       if user is None:
+           return render(request,'signin.htm', {
+            'form': AuthenticationForm,
+            'Error': 'Usuario o Contraseña es incorrecta'
+           })
+       else:
+           login(request, user)
+           return redirect('tasks')
+       
+@login_required
+def complete_task(request, task_id):
+    get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method =='POST':
+        Task.datecompleted = timezone.now()
+        Task.save()
+        return redirect('taks')
+    
+@login_required
+def delete_task(request, task_id):
+    get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method =='POST':
+        Task.delete()
+        return redirect('taks')
 
